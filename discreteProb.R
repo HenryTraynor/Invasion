4ab4de72555357f485ef37d49c5dfd39f1ccde63
Henry Traynor <henrytraynor@ecoadmins-iMac.local>
2022-10-04 15:56:58 -0400

    restructured for greater initial parms and matrix instead of data frame

--- discreteProb.R
+++ discreteProb.R
@@ -1,27 +1,32 @@
 # time scale: ?
 
-tau <- 5
+tau <- 0.5
 time <- 0
 time_max <- 100
 
-popSize.data <- data.frame(
-  popSize_name = c("Endemic", "Invader"),
-  individuals = c(initialN[1], initialN[2])
-)
+# (c(species i), c(species j))
+popSize <- matrix(c(initialN[1], initialN[2]), nrow = 1, ncol =2, byrow = TRUE)
+
+print(rpois(10, tau * b1 * popSize[nrow(popSize),2]))
 
 while (time<time_max) {
   # +/- terms
-  births1 = rpois(1, tau * b1 * popSize.data[1, ncol(popSize.data)])
-  births2 = rpois(1, tau * b2 * popSize.data[2, ncol(popSize.data)])
-  intraD1 = rpois(1, tau * b1 * (popSize.data[1, ncol(popSize.data)]^2))
-  intraD2 = rpois(1, tau * b2 * (popSize.data[2, ncol(popSize.data)]^2))
-  interD1 = rpois(1, tau * b1 * a12 * popSize.data[1, ncol(popSize.data)] * popSize.data[2, ncol(popSize.data)])
-  interD2 = rpois(1, tau * b2 * a21 * popSize.data[1, ncol(popSize.data)] * popSize.data[2, ncol(popSize.data)])
-  immigration1 = rpois(1, tau * d1 * popSize.data[1, ncol(popSize.data)])
-  immigration2 = rpois(1, tau * d2 * popSize.data[2, ncol(popSize.data)])
+  births1 = rpois(1, tau * b1 * popSize[nrow(popSize), 1])
+  births2 = rpois(1, tau * b2 * popSize[nrow(popSize), 2])
+  intraD1 = rpois(1, tau * b1 * (popSize[nrow(popSize),1]^2))
+  intraD2 = rpois(1, tau * b2 * (popSize[nrow(popSize),2]^2))
+  interD1 = rpois(1, tau * b1 * a12 * popSize[nrow(popSize),1] * popSize[nrow(popSize),2])
+  interD2 = rpois(1, tau * b2 * a21 * popSize[nrow(popSize),1] * popSize[nrow(popSize),2])
+  immigration1 = rpois(1, tau * d1 * popSize[nrow(popSize),1])
+  immigration2 = rpois(1, tau * d2 * popSize[nrow(popSize),2])
   
-  popSize.data$individuals <- c(popSize.data[1, ncol(popSize.data)]+ births1 - intraD1 - interD1 + immigration1,
-                                popSize.data[2, ncol(popSize.data)]+ births2 - intraD2 - interD2 + immigration2,)
+  nextStep <- c(popSize[nrow(popSize), 1]+ births1 - intraD1 - interD1 + immigration1,
+                popSize[nrow(popSize), 2]+ births2 - intraD2 - interD2 + immigration2)
+  
+  popSize <- rbind(popSize, nextStep)
   
   time = time + tau
 }
+
+plot(popSize[, 1], popSize[, 2])
+
--- initialCon.R
+++ initialCon.R
@@ -1,24 +1,32 @@
+# function for continuous analysis (maybe place in 'contModel' script?)
 LVcomp <- function (t, n, parms) {
   with(as.list(parms), {
-    dn1dt <- birth1 * (1 - (n[1]/k1) - a12 * (n[2]/k1))
+    dn1dt <- b1 * (1 - (n[1]/k1) - a12 * (n[2]/k1))
     dn2dt <- b2 * n[2] * (1 - (n[2]/k2) - a21 * (n[1]/k2))
     list(c(dn1dt, dn2dt))
   })
 }
 
-initialAtt <- c(b1 = 0.2,
-                b2 = 0.3,
-                k1 = 10,
-                k2 = 10,
-                a12 = 0.1,
-                a21 = 0.5)
+# initial attributes for species interactions
+initialAtt <- c(b1 = 20,
+                b2 = 30,
+                k1 = 100000,
+                k2 = 100000,
+                a12 = 0.2,
+                a21 = 0.6,
+                delta1 = 0.0,
+                delta2 = 0.3)
 
-birth1 <- initialAtt[1]
-birth2 <- initialAtt[2]
+# for access in 'discreteProb' script
+b1 <- initialAtt[1]
+b2 <- initialAtt[2]
 k1 <- initialAtt[3]
 k2 <- initialAtt[4]
 a12 <- initialAtt[5]
 a21 <- initialAtt[6]
+d1 <- initialAtt[7]
+d2 <- initialAtt[8]
 
-initialN <- c(10,
-              1)
+# initial population size: (species 1, species2)
+initialN <- c(1000,
+              10)
