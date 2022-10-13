72cb35297de5bad574a1559da3dc4070f7e11380
Henry Traynor <henrytraynor@ecoadmins-iMac.local>
2022-10-13 12:38:06 -0400

    parameters with high separation and endemic extinction

--- initialCon.R
+++ initialCon.R
@@ -1,23 +1,19 @@
-# initial attributes for species interactions
-initialAtt <- c(b1 = .05,
-                b2 = .07,
-                k1 = 100,
-                k2 = 100,
-                a12 = 0.6,
-                a21 = 0.3,
-                delta1 = 0.0,
-                delta2 = 0.3)
+# time scale: years
 
-# for access in 'discreteProb' script
-b1 <- initialAtt[1]
-b2 <- initialAtt[2]
-k1 <- initialAtt[3]
-k2 <- initialAtt[4]
-a12 <- initialAtt[5]
-a21 <- initialAtt[6]
-d1 <- initialAtt[7]
-d2 <- initialAtt[8]
+# initial attributes for species interactions
+att.param <- c(b1 = .12,
+               b2 = .3,
+               k1 = 700,
+               k2 = 900,
+               a12 = 1.5,
+               a21 = 0.8)
+
+#time parameters (tau, time_max)
+time.param <- c(1/52,
+                100)
 
 # initial population size: (species 1, species2)
-initialN <- c(100,
-              20)
+initial.N <- c(700,
+               10)
+
+
