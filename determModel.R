38e220188eea0f7c1108be67cf9adb589823d51e
Henry Traynor <henrytraynor@ecoadmins-iMac.local>
2022-10-13 12:39:08 -0400

    determModel is now probModel w/o rpois distribution to allow for function creation and easier ggplot input

--- determModel.R
+++ determModel.R
@@ -1,13 +1,41 @@
-library(deSolve)
-
-# function for continuous analysis (maybe place in 'contModel' script?)
-LVcomp <- function (t, n, parms) {
-  with(as.list(parms), {
-    dn1dt <- b1 * n[2] * (1 - (n[1]/k1) - a12 * (n[2]/k1))
-    dn2dt <- b2 * n[2] * (1 - (n[2]/k2) - a21 * (n[1]/k2))
-    list(c(dn1dt, dn2dt))
-  })
+determModel <- function(initialN, attParms, timeParms) {
+  tau <- timeParms[1]
+  time_max <- timeParms[2]
+  time <- 0
+  #indexing
+  step <- 1
+  #for df initialization
+  numStep <- as.integer(time_max/tau)
+  
+  #creates df with time and population sizes (0,0)
+  df.pop <- data.frame(time=seq(0,time_max, by=tau),
+                       endemic=vector("integer", numStep+1),
+                       invader=vector("integer", numStep+1))
+  
+  #define initial state
+  df.pop[1, 2:3] <- initialN
+  
+  
+  while(time<time_max) {
+    step <- step+1
+    #populations at current timestep as vector
+    pops <- as.numeric(df.pop[step-1,2:3])
+    
+    #births
+    birth <- tau*c(attParms[1:2])*pops[1:2]
+    #intra-species comp deaths
+    intra.death <- tau*c(attParms[1:2])/c(attParms[3:4])*pops[1:2]^2
+    #inter-species comp deaths
+    inter.death <- tau*c(attParms[1:2])*c(attParms[5:6])/c(attParms[3:4])*pops[1]*pops[2]
+    #delta(pops)
+    change <- birth-intra.death-inter.death
+    
+    #check for double counting
+    
+    #updating population data in df
+    df.pop[step,2:3] <- pops[1:2] + change[1:2]
+    
+    time=time+tau
+  }
+  return(df.pop)
 }
-
-out <- ode(y = initialN, times = 1:100, func = LVcomp, parms = initialAtt)
-matplot(out[, 1], out[, -1], type = "l") 
