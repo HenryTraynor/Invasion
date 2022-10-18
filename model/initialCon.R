eb1f4925549f7939b4807a2a86c792ef4f97c70a
Henry Traynor <henrytraynor@ecoadmins-iMac.local>
2022-10-18 12:19:12 -0400

    appended time_invade to time.param

--- model/initialCon.R
+++ model/initialCon.R
@@ -4,15 +4,16 @@
 att.param <- c(b1 = .12,
                b2 = .3,
                k1 = 700,
-               k2 = 750,
+               k2 = 800,
                a12 = 1.1,
                a21 = 0.9,
                del1 = 0.0,
-               del2 = 5)
+               del2 = 2)
 
-#time parameters (tau, time_max)
+#time parameters (tau, time_max, time_invade)
 time.param <- c(1/52,
-                150)
+                150,
+                10)
 
 # initial population size: (species 1, species2)
 initial.N <- c(700,
