2734f2122a8fe0f0882257227ac1257ae90e3d45
Henry Traynor <henrytraynor@ecoadmins-iMac.local>
2022-10-25 11:49:30 -0400

    addition of time.interval parameters

--- model/initialCon.R
+++ model/initialCon.R
@@ -13,8 +13,11 @@ att.param <- c(b1 = .25,
 #time parameters (tau, time_max, time_invade)
 time.param <- c(1/52,
                 150,
-                10)
+                0)
 
 # initial population size: (species 1, species2)
 initial.N <- c(700,
                0)
+
+# (window, step) - step must be a multiple of tau
+time.interval <- c(1, 8/52) #(1 year interval, 8 weeks)
