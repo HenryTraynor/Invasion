37a834dc1d75638af246ff5f17a659828e4698ee
Henry Traynor <henrytraynor@ecoadmins-iMac.local>
2022-10-27 13:48:55 -0400

    now computes stan-dev but times are not properly indexed for graphing and windows are not overlapping

--- intervalAnalysis.R
+++ intervalAnalysis.R
@@ -1,34 +1,21 @@
-intervalAnalysis <- function(df.sample, time.interval) {
-  time.interval <- time.interval
-  df.sample <- df.sample
+library(zoo)
+
+intervalAnalysis <- function(df.sample, time.param) {
+  tau <- time.param$tau
+  time.window <- time.param$time.window
+  window.step <- time.param$window.step
+  time.max <- time.param$time.max
   
-  step <- 1 #index of df.sd
-  time.step <- time.interval[2] 
-  time <- 0
-  time.window <- time.interval[1]
-  time.max <- time.param[2]
   
-  window <- 0 #index for sd calculation in sample df
-  window.step <- time.interval[2]/time.param[1]
+  df.sd <- rollapply(df.sample[,2:3],
+                    FUN="sd",
+                    width=time.window/tau,
+                    by = window.step/tau,
+                    by.column = TRUE,
+                    align = "right")
   
-  df.sample <- df.popD
+  df.sd <- data.frame(time=seq(0, time.max, by), df.sd)
   
-  num.interval <- as.integer((time.max-time.window)/time.step)
-  
-  df.sd <- data.frame(interval = seq(time.step,time.max-time.window,by=time.step-1),
-                      endemic = vector("double", num.interval-1),
-                      invader = vector("double", num.interval-1))
-  
-  while (step<length(df.sample$endemic)) {
-    window <- window+window.step
-    
-    start.window <- window-window.step
-    df.sd$endemic[step] <- sd(df.sample$endemic[start.window:window])
-    df.sd$invader[step] <- sd(df.sample$endemic[start.window:window])
-    
-    step <- step+1
-    time <- time+time.step
-  }
-  
-  return(df.sd)
+  return(as.data.frame(df.sd))
 }
+
