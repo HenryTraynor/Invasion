63dc819735e37e3c120103d6083fa69144a6f7e1
Henry Traynor <henrytraynor944@gmail.com>
2022-11-01 14:27:44 -0400

    working sd calculation with correctly indexed df output; non-overlapping intervals

--- intervalAnalysis.R
+++ intervalAnalysis.R
@@ -1,21 +1,23 @@
 library(zoo)
 
-intervalAnalysis <- function(df.sample, time.param) {
+intervalStanDev <- function(df.sample, time.param) {
+  #unpacking
   tau <- time.param$tau
   time.window <- time.param$time.window
   window.step <- time.param$window.step
   time.max <- time.param$time.max
   
-  
-  df.sd <- rollapply(df.sample[,2:3],
+  #calculation of standard devation values
+  sd <- rollapply(df.sample[,2:3],
                     FUN="sd",
                     width=time.window/tau,
                     by = window.step/tau,
                     by.column = TRUE,
                     align = "right")
   
-  df.sd <- data.frame(time=seq(0, time.max, by), df.sd)
+  #combining sd values with time indices
+  sd <- data.frame(time=seq(time.window, time.max, by = window.step), sd)
   
-  return(as.data.frame(df.sd))
+  return(sd)
 }
 
