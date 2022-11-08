9e13020cb36c0ef824d4ec3228fd51a0b6ee8966
Henry Traynor <henrytraynor944@gmail.com>
2022-11-08 11:20:23 -0500

    negligible

--- model/modelParam.R
+++ model/modelParam.R
@@ -3,18 +3,18 @@
 # initial attributes for species interactions
 att.param <- list(n1 = 700,
                   n2 = 0,
-                  b1 = .2,
+                  b1 = .3,
                   b2 = .3,
                   k1 = 700,
-                  k2 = 900,
-                  a12 = 1.2,
-                  a21 = 0.8,
-                  del1 = 0.0,
-                  del2 = 3)
+                  k2 = 700,
+                  a12 = 0,
+                  a21 = 1,
+                  del1 = 0,
+                  del2 = 4)
 
 #time parameters
 time.param <- list(tau = 1/52,
-                   time.max = 100,
+                   time.max = 250,
                    time.invade = 0,
                    time.window = 8/52,
                    window.step = 1)
