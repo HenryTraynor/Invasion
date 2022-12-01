7670c01f155f02d66b249d3f8f26c77d93b4b8b0
Henry Traynor <henrytraynor944@gmail.com>
2022-12-01 13:22:31 -0500

    better params for bifurcation analysis

--- model/modelParam.R
+++ model/modelParam.R
@@ -2,19 +2,19 @@
 
 # initial attributes for species interactions
 att.param <- list(n1 = 700,
-                  n2 = 0,
+                  n2 = 5,
                   b1 = .3,
-                  b2 = .3,
+                  b2 = .5,
                   k1 = 700,
                   k2 = 700,
-                  a12 = 0,
-                  a21 = 1,
+                  a12 = 1,
+                  a21 = 0.9,
                   del1 = 0,
-                  del2 = 4)
+                  del2 = 0)
 
 #time parameters
 time.param <- list(tau = 1/52,
-                   time.max = 250,
+                   time.max = 175,
                    time.invade = 0,
                    time.window = 8/52,
                    window.step = 1)
