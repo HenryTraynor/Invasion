fb5850e796084fbd7e55ad3a8dacf8648f8545c4
Henry Traynor <henrytraynor@ecoadmins-iMac.local>
2022-12-12 16:21:58 -0500

    switch to change in a21 instead of a12 so that a21 can increase over 1; worked once; receiving logical error

--- increasingAlpha.R
+++ increasingAlpha.R
@@ -7,8 +7,10 @@ alphaSim <- function(att.param, time.param, do.prob = TRUE, ratio.max) {
   b2 <- att.param$b2
   k1 <- att.param$k1
   k2 <- att.param$k2
-  a12 <- 0
-  a21 <- att.param$a21
+  a12 <- 1
+  a21 <- 1
+  a11 <- att.param$a11
+  a22 <- att.param$a22
   del1 <- att.param$del1
   del2 <- att.param$del2
   #time params
@@ -33,18 +35,20 @@ alphaSim <- function(att.param, time.param, do.prob = TRUE, ratio.max) {
   #define initial state
   df.pop[1, 2:3] <- c(n1,n2)
   
+  
   #a12 for each step
-  a12.set <- seq(0,ratio.max,by=ratio.max/num.step)
+  ratio.set <- seq(0,ratio.max,by=ratio.max/num.step)
+  ratio.set[1] <- ratio.set[2]/2
   
   while(time<time.max) {
-    a12 <- a12.set[step]
+    a21 <- 1/ratio.set[step]
     step <- step+1
     #populations at current timestep as vector
     pops <- as.numeric(df.pop[step-1,2:3])
     
     #events are defined for either boolean input 
     birth <- tau*c(b1,b2)*pops
-    intra.death <- tau*c(b1,b2)/c(k1,k2)*pops^2
+    intra.death <- tau*c(b1,b2)*c(a11,a22)/c(k1,k2)*pops^2
     inter.death <- tau*c(b1,b2)*c(a12,a21)/c(k1,k2)*pops[1]*pops[2]
     
     #now redefined in the case of probabilistic model
@@ -66,8 +70,18 @@ alphaSim <- function(att.param, time.param, do.prob = TRUE, ratio.max) {
       immigration <- 0
     }
     
+    #if((time/tau)%%156==0) {
+    #  immigration <- 20
+    #}
+    
     #new entry in df
     change <- birth-intra.death-inter.death+immigration
+    if (change[1] > pops[1] && change[1]<0) {
+      change[1] <- 0
+    } 
+    if (change[2] > pops[2] && change[2]<0) {
+      change[2] <- 0
+    }
     df.pop[step,2:3] <- pops+change
     
     #step
@@ -76,11 +90,11 @@ alphaSim <- function(att.param, time.param, do.prob = TRUE, ratio.max) {
   return(df.pop)
 }
 
-df.alphaSim <- alphaSim(att.param, time.param, ratio.max=1.5)
+df.alphaSim <- alphaSim(att.param, time.param, ratio.max=2)
 
-ggp1 <- ggplot(data=df.alphaSim, aes_(x=df.alphaSim[,1], y=df.alphaSim[,3], color='Invader - Probabilistic')) +
+ggp1 <- ggplot(data=df.alphaSim, aes_(x=df.alphaSim[,1], y=df.alphaSim[,2], color='Endemic - Probabilistic')) +
   geom_line() +
-  geom_line(data=df.alphaSim, aes_(x=df.alphaSim[,1], y=df.alphaSim[,2], color='Endemic - Probabilistic')) +
+  geom_line(data=df.alphaSim, aes_(x=df.alphaSim[,1], y=df.alphaSim[,3], color='Invader - Probabilistic')) +
   ggtitle('Endemic and Invader Species Abundance versus Time') +
   xlab('time (years)') + ylab('abundance') +
   theme(legend.position = "bottom")
@@ -91,15 +105,18 @@ ggp2 <- ggplot(data=df.sd, aes_(x=df.sd[,1], y=df.sd[,3], color='SD: invader')) 
   geom_line()  +
   ggtitle('Invader Interval Standard Deviations') +
   xlab('time (years)') + ylab('number of individuals') +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom") +
+  coord_cartesian(xlim = c(0, time.param$time.max))
 
 df.var <- intervalStanDev(df.alphaSim, time.param, fun.call="var")
 
 ggp3 <- ggplot(data=df.var, aes_(x=df.var[,1], y=df.var[,3], color='var: invader')) +
   geom_line() +
-  ggtitle('Invader Interval Standard Deviations') +
+  ggtitle('Invader Interval Variance') +
   xlab('time (years)') + ylab('number of individuals') +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom") +
+  coord_cartesian(xlim = c(0, time.param$time.max))
 
 
-grid.arrange(ggp1, ggp2, ggp3, ncol=1)
+grid.arrange(ggp1, ncol=1)
+
