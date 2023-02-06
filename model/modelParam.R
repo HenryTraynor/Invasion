26d3ad9cb8b3ab222fdeb46525c39c8f8f9150f2
Henry Traynor <henrytraynor@ecoadmins-iMac.local>
2023-02-06 18:02:30 -0500

    neg.

--- model/modelParam.R
+++ model/modelParam.R
@@ -1,22 +1,22 @@
 # time scale: years
 
 # initial attributes for species interactions
-att.param <- list(n1 = 500,
+att.param <- list(n1 = 200,
                   n2 = 0,
-                  b1 = 0.1,
-                  b2 = 0.3,
-                  k1 = 500,
-                  k2 = 500,
+                  b1 = 0.8,
+                  b2 = 1.0,
+                  k1 = 100,
+                  k2 = 100,
                   a12 = 1,
                   a21 = 2,
-                  a11 = 1,
+                  a11 = 0.6,
                   a22 = 1,
                   del1 = 0,
                   del2 = 2)
 
 #time parameters
-time.param <- list(tau = 1/104,
-                   time.max = 150,
+time.param <- list(tau = 1/52,
+                   time.max = 400,
                    time.invade = 0,
                    time.window = 2.5,
                    window.step = 0.5)
