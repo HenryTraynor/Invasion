985856e6b4e8eb341fed8851d6ab7b806c5052b5
Henry Traynor <henrytraynor944@gmail.com>
2024-01-11 12:29:39 -0500

    created for bug testing

---
+++ playground.R
@@ -0,0 +1,18 @@
+library(ggplot2)
+library(bit64)
+time.param <- list(tau = 1/365,
+                   time.invade = 0,
+                   time.window = 70,
+                   window.step = 3)
+ttb.test <- 100
+
+df.data <- alphaSim(att.param, time.param, ratio.max=2, ttb=ttb.test)
+
+test.plot <- ggplot(df.data, aes_(x=df.data[,1], y=df.data[,2])) +
+  geom_line() +
+  ggtitle('Species Abundance versus Time') +
+  xlab('time (years)') + ylab('abundance') +
+  theme(legend.position = "bottom") +
+  geom_vline(xintercept=ttb.test, linetype='dashed', color='black')
+
+test.plot
