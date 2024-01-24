10e7d9e9ce22abfd0b6f1e66126fff51bd056ad7
Henry Traynor <henrytraynor@ecoadmins-iMac-2.local>
2024-01-24 12:41:36 -0500

    na

--- playground.R
+++ playground.R
@@ -6,7 +6,7 @@ time.param <- list(tau = 1/365,
                    window.step = 3)
 ttb.test <- 25
 
-df.data <- alphaSim(att.param, time.param, ratio.max=2, ttb=ttb.test, do.fail=FALSE)
+df.data <- alphaSim(att.param, time.param, ratio.max=0.5, ttb=ttb.test, do.fail=TRUE)
 
 test.plot <- ggplot(df.data, aes(x=df.data[,1], y=df.data[,2])) +
   geom_line() +
@@ -23,23 +23,27 @@ pass_fail <- halfAndHalf(ttb=5,
                          failSimulations=3)
 
 #------------------------------------
-passSimulations <- 3
-failSimulations <- 3
+halfSimulations <- 3
 
-pass_sim_list = replicate(n=passSimulations,
+
+pass_sim_list = c(replicate(n=passSimulations,
                           expr=alphaSim(att.param,time.param,ratio.max=2, ttb=5,do.fail=FALSE),
-                          simplify=F)
-fail_sim_list = replicate(n=failSimulations,
+                          simplify=F))
+fail_sim_list = c(replicate(n=failSimulations,
                           expr=alphaSim(att.param,time.param,ratio.max=2, ttb=5,do.fail=TRUE),
-                          simplify=F)
-passSimulations <- 3
-failSimulations <- 3
+                          simplify=F))
 
-data.list <- cbind(pass_sim_list,fail_sim_list)
+df.data <- data.frame(success=c(rep("pass", halfSimulations),rep("fail", halfSimulations)),
+                      data=vector(length=halfSimulations*2))
+
+for(i in 1:halfSimulations) {
+  df.data$data[i]=pass_sim_list[i]
+  df.data$data[i+halfSimulations]=fail_sim_list[i]
+}
+
+df.data = as.data.frame(data.list)
 
 df.output = data.frame(success=c(rep("pass",passSimulations),rep("fail", failSimulations)))
-df.output = cbind(t(df.output),data.list)
-
 
 
 
