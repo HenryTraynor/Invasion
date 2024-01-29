7b8044aaae28f9ab49e67dfa893cc57b18142069
Henry Traynor <henrytraynor@ecoadmins-iMac-2.local>
2024-01-29 13:30:43 -0500

    script that returns df with threshold, TPR, and FPR

---
+++ statistics/ROCcurveData.R
@@ -0,0 +1,37 @@
+library(DescTools)
+
+ROCcurveData <- function(df.input, numThresholds) {
+  min = df.input$stat[which.min(df.input$stat)]
+  max = df.input$stat[which.max(df.input$stat)]
+  thresholds = seq(min-0.01, max+(max*.01), length.out=numThresholds)
+  
+  df.rates = data.frame(threshold=thresholds,
+                         TPR=vector("numeric", length=length(thresholds)),
+                         FPR=vector("numeric", length=length(thresholds)))
+  # iterate through all thresholds
+  for(i in 1:length(thresholds)) {
+    # now finding TPR and FPR for a single threshold
+    truePos=0
+    falsePos=0
+    pos=0
+    neg=0
+    # iterate through each realization from halfAndHalf input
+    for(j in 1:length(df.input$do.win)) {
+      if (df.input$do.win[j]) {
+        pos=pos+1
+        if(df.input$stat[j]>thresholds[i]) {
+          truePos=truePos+1
+        }
+      }
+      else {
+        neg=neg+1
+        if(df.input$stat[j]>thresholds[i]) {
+          falsePos=falsePos+1
+        }
+      }
+    }
+    df.rates$TPR[i] = truePos/pos
+    df.rates$FPR[i] = falsePos/neg
+  }
+  return(df.rates)
+}
